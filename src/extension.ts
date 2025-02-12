cb45b5d5a1fa29efed16764d3944026f9363d53a
DY Dev <dydev420@gmail.com>
2025-02-12 12:26:04 +0530

    launch  webview on startup for ez dev

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,9 @@ import { ChatPanel } from './panels/ChatPanel';
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "vs-gpt-chat" is now active!');
+	
+	ChatPanel.render(context.extensionUri);
+	console.log('TEMP: rendering finished on startup');
 
 	// The command has been defined in the package.json file
 	// The commandId parameter must match the command field in package.json
