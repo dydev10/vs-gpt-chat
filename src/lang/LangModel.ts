1b4ca26cafdeddacf9e7fb58e3bfb43619af31ff
DY Dev <dydev420@gmail.com>
2025-02-13 02:11:32 +0530

    added db connection with seed data script

--- src/lang/LangModel.ts
+++ src/lang/LangModel.ts
@@ -1,16 +1,35 @@
-import { ChatOllama } from "@langchain/ollama";
+import { ChatOllama, OllamaEmbeddings } from "@langchain/ollama";
+// import { PromptTemplate } from '@langchain/core/prompts';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import VectorDBService from "./VectorDBService";
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
 
 class LangModel {
+  docSources: string[];
   modelName: string;
   model: ChatOllama;
+  splitter: RecursiveCharacterTextSplitter;
+  vectorDBService: VectorDBService; 
 
   constructor(modelName: string = 'deepseek-r1:7b') {
     this.modelName = modelName;
-    
+    this.docSources = [
+      'https://github.com/aseprite/aseprite/blob/main/docs/ase-file-specs.md',
+      'https://www.aseprite.org/api',
+      'https://www.aseprite.org/api/app#app',
+    ];
+
     this.model = new ChatOllama({
       baseUrl: 'http://localhost:11434',
       model: 'deepseek-r1:7b',
     });
+
+    this.splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 512,
+      chunkOverlap: 100,
+    });
+
+    this.vectorDBService = new VectorDBService();       
   }
 
   // TODO: fix stream typing to make it arg
@@ -21,6 +40,84 @@ class LangModel {
       stream: true,
     });
     return response;
+  };
+
+  scrapPage = async (url: string): Promise<string> => {
+    console.log('----Call Scrapper here.... url:--:', url);
+
+    const loader = new PuppeteerWebBaseLoader(url, {
+      launchOptions: {
+        headless: true,
+      },
+      gotoOptions: {
+        waitUntil: 'domcontentloaded',
+      },
+      evaluate: async (page, browser) => {
+        const result = await page.evaluate(() => document.body.innerHTML);
+        await browser.close();
+        return result; 
+      },
+    });
+
+    const scrapped =(await loader.scrape())?.replace(/<[^>]*>?/gm, '')
+
+    console.log('Scrapped');
+    
+
+    // TODO: only keeps text content, change regex to include html and code
+    return scrapped;
+  }
+
+  createSampleCollection = async () => {
+    return await this.vectorDBService.createCollection()
+      .then(() => {
+        return this.loadSampleData();
+      });
+  };
+
+  /**
+   * [SEED] only called once on install for seed data
+   */
+  setupSample = () => {
+    this.createSampleCollection().then(() => {
+      console.log('DONE setup Samples');
+    });
+  };
+
+  loadSampleData = async () => {
+    const collection = await this.vectorDBService.getCollection();
+    for await (const url of this.docSources) {
+      const content = await this.scrapPage(url);
+      const chunks = await this.splitter.splitText(content);
+              
+      const embeddings = new OllamaEmbeddings({
+        baseUrl: 'http://localhost:11434',
+        // model: 'text-embedding-3-small',  //  dimension =  1536
+        model: 'nomic-embed-text',  //  dimension = 768
+        // input: chunk,
+      });
+      embeddings.client.pull({
+        model: 'nomic-embed-text',
+      })
+      for await (const chunk of chunks) {        
+        try {
+          console.log("AWAITING embeddings...");
+          const vector = await embeddings.embedQuery(chunk);
+          console.log('Vectors length from embed', vector.length);
+
+          const res = await collection.insertOne({
+            $vector: vector,
+            text: chunk,
+          });
+          console.log('Db write chunk res', res);
+        } catch (error) {
+          console.log('OHno', error);
+          throw error;
+        }
+      }
+    }
+
+    return collection;
   };
 }
 
---
+++ src/lang/VectorDBService.ts
@@ -0,0 +1,65 @@
+import { DataAPIClient, Db } from "@datastax/astra-db-ts";
+
+export type SimilarityMetric = 'dot_product' | 'cosine' | 'euclidean';
+
+class VectorDBService {
+  client: DataAPIClient;
+  db: Db
+
+  constructor() {
+    const {
+      ASTRA_DB_TOKEN,
+      ASTRA_DB_NAMESPACE,
+      ASTRA_DB_COLLECTION,
+      ASTRA_DB_URL,
+    } = process.env;
+
+    console.log('DB Init', {
+      ASTRA_DB_TOKEN,
+      ASTRA_DB_NAMESPACE,
+      ASTRA_DB_COLLECTION,
+      ASTRA_DB_URL,
+    });
+    
+
+    this.client = new DataAPIClient(ASTRA_DB_TOKEN);  
+    this.db = this.client.db(
+      ASTRA_DB_URL as string,
+      {
+        namespace: ASTRA_DB_NAMESPACE,
+      },
+    );
+  }
+
+  createCollection = async (similarityMetric: SimilarityMetric = 'dot_product') => {
+    const {
+      ASTRA_DB_COLLECTION,
+    } = process.env;
+
+    if (ASTRA_DB_COLLECTION) {
+      console.log('Called create conn');
+      const conn = await this.db.createCollection(
+        ASTRA_DB_COLLECTION,
+        {
+          vector: {
+            // dimension: 1536,  //  for text-embedding-3-small
+            dimension: 768,  // for nomic-embed-text
+            metric: similarityMetric,
+          }
+        },
+      );
+
+      console.log("DB CONNECTION", conn);
+      return conn;
+      
+    } else {
+      throw new Error('No Collection name defined in .env file')
+    }
+  };
+
+  getCollection = async (collectionName: string = process.env.ASTRA_DB_COLLECTION as string) => {
+    return this.db.collection(collectionName);
+  };
+}
+
+export default VectorDBService;
