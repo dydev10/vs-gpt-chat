e05c2eecc2563a86e93ffd3edfcdbec4b146a051
DY Dev <dydev420@gmail.com>
2025-02-14 12:17:52 +0530

    random: add jsx, tsx prism config

--- webview-ui/vite.config.ts
+++ webview-ui/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     prism({
-      languages: ['javascript', 'css', 'html', 'typescript'],
+      languages: ['javascript', 'css', 'html', 'typescript', 'jsx', 'tsx'],
       plugins: ['line-numbers'],
       theme: 'tomorrow',
       css: true,
